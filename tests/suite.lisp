0f8d266a7aa13f9c24a1eedc866689d9a0438a71
Matze <matze@matzsoft.de>
2013-10-01 20:16:46 +0200

    tests

---
+++ tests/suite.lisp
@@ -0,0 +1,9 @@
+(defpackage :defrest-test (:use :defrest :fiveam :drakma))
+
+(in-package :defrest-test)
+
+(def-suite defrest-tests :description "Unit Tests for defrest")
+
+(in-suite defrest-tests)
+
+
